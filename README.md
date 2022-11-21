a56fc2e9212e3bcef14965c9bfb11c69d182bf7e
JJ Small <jjsmall009@gmail.com>
2022-11-20 20:46:15 -0800

    Study page layout done; level picker works great

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ A simple and clean kanji flashcard web app
 - [x] Do some layout sketches on Figma
 - [x] Brainstorm feature set and website structure
 - [ ] Get kanji data from WaniKani (JSON most likely)
-- [ ] Build the level picker section and add functionality 
+- [x] Build the level picker section and add functionality 
 
 Happy coding!
--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
 // Kanji Flash - 2022 JJ Small
+
+// Create the level selector buttons
+const container = document.getElementById("level-picker");
+
+function makeRows(rows, cols) {
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.innerText = (i + 1);   
+        cell.addEventListener("click", level_select);
+        container.appendChild(cell).className = "level-btn";
+    }
+}
+
+makeRows(15, 4);
+
+// Level button functionality
+function level_select() {
+    const level = this.innerText;
+    const scoreline = document.getElementById("level-score");
+    scoreline.innerText = `Level ${level} - 0/23`;
+}
+
+// Answer button functionality
+let answer_btns = document.getElementsByClassName("answer-btn");
+for (var i = 0; i < answer_btns.length; i++) {
+    answer_btns[i].addEventListener("click", processAnswer);
+}
+
+function processAnswer() {
+    alert("you clicked and answer button!");
+}
