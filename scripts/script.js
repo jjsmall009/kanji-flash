d8602cea5c3f62cddbc65fa58d4e7debd2496602
JJ Small <jjsmall009@gmail.com>
2022-11-27 21:36:04 -0800

    Refactored the javascript using a level object and other tweaks

--- scripts/script.js
+++ scripts/script.js
@@ -1,115 +1,111 @@
-// Kanji Flash - 2022 JJ Small
+// Kanji Flash - JJ Small 2022
 
-/* 
-* LEVELS ZONE - Populates the levels zone with div buttons using CSS Grid
+/* ============================================
+    Grab page elements and other initialization
+   ============================================
 */
-const levelZone = document.getElementById("level-picker");
+let levelZone = document.getElementById("level-picker");
+let scoreline = document.getElementById("level-scoreline");
+let card = document.getElementById("card-content");
+let goodBtn = document.getElementById("good-btn");
+let badBtn = document.getElementById("bad-btn");
+let currentLevel = null;
 
+goodBtn.addEventListener("click", updateGood);
+badBtn.addEventListener("click", updateBad);
+card.addEventListener("click", function () {
+  card.classList.toggle("is-flipped");
+});
+
+// Constructor to represent the current level
+function CurrentLevelData(level) {
+  this.level = level;
+  this.kanjiList = shuffle(data[level - 1][`${level}`]);
+  this.size = this.kanjiList.length;
+  this.correct = 0;
+}
+
+/* ============================================
+    Level Zone
+   ============================================
+*/
+// Populate the level zone with div buttons using CSS Grid
 function makeRows(rows, cols) {
-    for (let i = 0; i < (rows * cols); i++) {
-        let cell = document.createElement("div");
-        cell.innerText = (i + 1);   
-        cell.addEventListener("click", level_select);
-        levelZone.appendChild(cell).className = "level-btn";
-    }
+  for (let i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    cell.innerText = i + 1;
+    cell.addEventListener("click", levelSelect);
+    levelZone.appendChild(cell).className = "level-btn";
+  }
 }
 makeRows(15, 4);
 
+// Selecting a level will create a new level object and set things up
+function levelSelect() {
+  currentLevel = new CurrentLevelData(this.innerText);
+  updateScoreline();
+  updateKanjiCard(currentLevel.kanjiList[0]);
+}
 
-/* 
-* CARD FLIP - Clicking on the kanji card will flip it over to reveal the readings/meanings
+/* ============================================
+    Kanji Card and Flashcard Zone
+   ============================================
 */
-let card = document.querySelector('.card-content');
-card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-    goodBtn.disabled = false;
-    badBtn.disabled = false;
-});
-
-/* 
-* Event handling for clicking a level button
-* Will grab that levels kanji and queue them up for flashcarding
-*/
-let size = 0;
-let level = -1;
-let correct = 0;
-function level_select() {
-    correct = 0;
-    level = this.innerText
-    const kanjiList = data[level - 1][`${level}`]
-    size = kanjiList.length;
-    update_label(level, 0, size);
-    create_flashcard_queue(kanjiList);
+function updateScoreline() {
+  const level = currentLevel.level;
+  const correct = currentLevel.correct;
+  const size = currentLevel.size;
+  scoreline.innerText = `Level ${level} - ${correct}/${size}`;
 }
 
-function update_label(level, correct, num_kanji) {
-    const scoreline = document.getElementById("level-scoreline");
-    scoreline.innerText = `Level ${level} - ${correct}/${num_kanji}`;
+// Helper function to randomize the kanji set
+function shuffle(arr) {
+  var j, x, index;
+  for (index = arr.length - 1; index > 0; index--) {
+    j = Math.floor(Math.random() * (index + 1));
+    x = arr[index];
+    arr[index] = arr[j];
+    arr[j] = x;
+  }
+  return arr;
 }
 
-let kanjiQueue = [];
-function create_flashcard_queue(list) {
-    kanjiQueue = shuffle(list);
-    update_kanji_card(list[0]);
+// Update the front and back of the kanji card
+function updateKanjiCard(kanjiData) {
+  let kanjiBig = document.getElementById("kanji_big");
+  let kanjiSmall = document.getElementById("kanji_small");
+  let readings = document.getElementById("readings");
+  let meanings = document.getElementById("meanings");
+
+  kanjiBig.innerText = kanjiData.kanji;
+  kanjiSmall.innerText = kanjiData.kanji;
+
+  readings.innerText = kanjiData.readings.join(", ");
+  meanings.innerText = kanjiData.meanings.join(", ");
 }
 
-function update_kanji_card(kanjiData) {
-    goodBtn.disabled = true;
-    badBtn.disabled = true;
-    let kanjiBig = document.getElementById("kanji_big");
-    let kanjiSmall = document.getElementById("kanji_small");
-    let readings = document.getElementById("readings");
-    let meanings = document.getElementById("meanings");
+// Button functionality
+function updateGood() {
+  currentLevel.kanjiList.shift();
+  card.classList.toggle("is-flipped");
+  currentLevel.correct += 1;
+  updateScoreline();
 
-    kanjiBig.innerText = kanjiData  .kanji;
-    kanjiSmall.innerText = kanjiData.kanji;
-
-    readings.innerText = kanjiData.readings.join(", ");
-    meanings.innerText = kanjiData.meanings.join(", ");
+  if (currentLevel.kanjiList.length == 0) {
+    reset();
+  } else {
+    updateKanjiCard(currentLevel.kanjiList[0]);
+  }
 }
 
-/* Helper function to randomize the kanji set */
-function shuffle (arr) {
-    var j, x, index;
-    for (index = arr.length - 1; index > 0; index--) {
-        j = Math.floor(Math.random() * (index + 1));
-        x = arr[index];
-        arr[index] = arr[j];
-        arr[j] = x;
-    }
-    return arr;
-}
-
-/* 
-* RESPONSE BUTTONS - Event handling for clicking the answer response buttons
-*/
-let goodBtn = document.getElementById("good-btn");
-let badBtn = document.getElementById("bad-btn");
-goodBtn.addEventListener("click", update_good);
-badBtn.addEventListener("click", update_bad);
-
-
-function update_good() {
-    kanjiQueue.shift();
-    card.classList.toggle('is-flipped');
-    correct += 1;
-    update_label(level, correct, size);
-
-    if(kanjiQueue.length == 0) {
-        reset();
-    } else {
-        update_kanji_card(kanjiQueue[0]);
-    }
-}
-
-function update_bad() {
-    wrong = kanjiQueue.shift();
-    kanjiQueue.push(wrong);
-    card.classList.toggle('is-flipped');
-    update_kanji_card(kanjiQueue[0]);
+function updateBad() {
+  wrongKanji = currentLevel.kanjiList.shift();
+  currentLevel.kanjiList.push(wrongKanji);
+  card.classList.toggle("is-flipped");
+  updateKanjiCard(currentLevel.kanjiList[0]);
 }
 
 function reset() {
-    alert("You've completed the level!");
-    correct = 0;
+  alert("You've completed the level!");
+  currentLevel = null;
 }
